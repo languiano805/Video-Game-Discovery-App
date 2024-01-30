149a10ee9637185bdcac5ae6f13180e603f80189
Anguinao, Leonardo <languiano@csus.edu>
2024-01-29 17:59:21 -0800

    fetching games from rawg api

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        Main
+        <GameGrid></GameGrid>
       </GridItem>
     </Grid>
   );

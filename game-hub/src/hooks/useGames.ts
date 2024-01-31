8859e2b6d75cc3535043ec853cd64cd9beec6ae3
Anguinao, Leonardo <languiano@csus.edu>
2024-01-30 16:09:05 -0800

    added loading skeletons

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -24,17 +24,22 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
-  
+    const[isLoading, setIsLoading] = useState(false);
+
     useEffect(() => {
         const controller = new AbortController();
 
-
+        setIsLoading(true)
       apiClient
         .get<FetchGameResponse>("/games", {signal: controller.signal})
-        .then((res) => setGames(res.data.results))
+        .then((res) => {
+            setGames(res.data.results)
+            setIsLoading(false)
+        })
         .catch((err) => {
             if(err instanceof CanceledError) return;
             setError(err.message);
+            setIsLoading(false)
         });
 
         return () => {
@@ -42,7 +47,7 @@ const useGames = () => {
         }
     }, []);
 
-    return { games, error };
+    return { games, error , isLoading};
 }
 
 export default useGames

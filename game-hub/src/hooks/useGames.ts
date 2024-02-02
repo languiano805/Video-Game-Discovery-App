a24fd7c243a7e3305294c8e0a2ce56bf61a99abc
Anguinao, Leonardo <languiano@csus.edu>
2024-02-01 16:31:41 -0800

    sort the games

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -2,26 +2,32 @@ import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
-export interface Platform{
-    id: number;
-    name: string;
-    slug: string;
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: {platform: Platform}[];
-    metacritic: number;
-  }
-  
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
 
+const useGames = (gameQuery: GameQuery) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+      },
+    },
 
-const useGames = (gameQuery: GameQuery) => useData<Game>("/games", {
-  params: {
-    genres: gameQuery.genre?.id, 
-    platforms: gameQuery.platform?.id}},
-    [gameQuery]);
+    [gameQuery]
+  );
 
-export default useGames
+export default useGames;
